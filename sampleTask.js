953760abaae12b71dbc7a37f085f51be641af7a2
muzaahmadk2 <muzaahmadk2@gmail.com>
2022-12-05 12:38:26 +0400

    fat arrow function

--- sampleTask.js
+++ sampleTask.js
@@ -30,20 +30,63 @@
 // var ageprint = print.bind(student);
 // ageprint();
 
-let mul = function(x,y){
-    console.log(x * y);
-}
-let multwo = mul.bind(this,2);
-multwo(3);
+// let mul = function(x,y){
+//     console.log(x * y);
+// }
+// let multwo = mul.bind(this,2);
+// multwo(3);
 
-//...................................................
+// //...................................................
 
-let multiply = function(x){
-    return function (y){
-        console.log(x * y);
-    }
-}
+// let multiply = function(x){
+//     return function (y){
+//         console.log(x * y);
+//     }
+// }
     
 
- let multiplytwo = multiply(2);
- multiplytwo(3);
+//  let multiplytwo = multiply(2);
+//  multiplytwo(3);
+
+
+class student{
+    static no = 0;
+    constructor(n,a,p,b){
+        this.name = n;
+        this.age = a;
+        this.phone = p;
+        this.mark = b;
+        student.no ++;
+    }
+    
+    eligible(x)
+    {
+        return (y)=>{
+            if(this.mark > x && this.age > y)
+                console.log(this.name+' is eligible');
+            else
+                console.log(this.name+' is not eligible');
+        }
+    }
+
+
+
+
+    static no_of_stud(){
+        console.log(this.no);
+    }
+}
+
+let s1 =new student('john',20,123,45);
+let s2 =new student('ram',20,124,45);
+let s3 =new student('kan',20,153,40);
+let s4 =new student('mann',20,183,35);
+let s5 =new student('ran',20,173,25);
+
+ s5.eligible(35)(19);
+ s1.eligible(35)(19);
+ s2.eligible(35)(19);
+ s3.eligible(35)(19);
+ s4.eligible(35)(19);
+//console.log(dd);
+//student.no_of_stud();
